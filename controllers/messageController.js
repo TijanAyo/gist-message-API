e93f4ecea8a135ff4a285854d9f8d9eca7999f98
TijanAyo <ayomidetijani6@gmail.com>
2022-02-25 00:16:07 +0100

    routes for CRUD

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -16,7 +16,31 @@ const status = (req, res)=>{
     }
 }
 
+const messages = (req, res)=>{
+    res.json({
+        message: 'Display all gisty messages here'
+    })
+}
+
+
+const create_message = (req, res)=>{
+    res.json({
+        message: 'Create your gisty messages here'
+    })
+}
+
+const update_message = (req, res)=>{
+    res.json({
+        message: 'update your gisty messages here'
+    })
+}
+
+const delete_message = (req, res)=>{
+    res.json({
+        message: 'delete your gisty messages here'
+    })
+}
 
 module.exports = {
-    status, welcome
+    status, welcome, messages, create_message, update_message, delete_message
 }
--- routes/messageRoute.js
+++ routes/messageRoute.js
@@ -1,15 +1,31 @@
 const express = require('express')
 const router = express.Router()
 
-const { status, welcome  } = require('../controllers/messageController')
+const { status, welcome, messages, create_message, update_message, delete_message  } = require('../controllers/messageController')
 
 // @desc: Home
 // @route: GET /
 router.get('/', welcome)
 
 // @desc: Checking status of API
-// @route: GET /api/status
+// @route: GET /api/v1/status
 router.get('/api/v1/status', status)
+
+// @desc: Display all messages
+// @route: GET /api/v1/messages
+router.get('/api/v1/messages', messages)
+
+// @desc: Create gisty messages
+// @route: POST /api/v1/messages
+router.post('/api/v1/messages', create_message)
+
+// @desc: Update messages
+// @route: PATCH /api/v1/messages/:id
+router.put('api/v1/messages/:id', update_message)
+
+// @desc: Delete messages
+// @route: Delete /api/v1/messages/:id
+router.delete('api/v1/messages/:id', delete_message)
 
 
 module.exports = router;
